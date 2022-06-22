bc11dcb38185b563a7a3eedf92ab5ab1f2bdf6a6
omprakashpandit <32616762+omprakashpandit@users.noreply.github.com>
2022-06-22 08:15:53 +0530

    message.properties file

---
+++ message.properties/PropsUtil.java
@@ -0,0 +1,16 @@
+package com.ilfs.util;
+
+import java.util.ResourceBundle;
+
+public class PropsUtil {
+	public static String getPropertyKey(String key) {
+		ResourceBundle rb;
+		try {
+			rb = ResourceBundle.getBundle("message");
+			key = (String) rb.getString(key);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return key.trim();
+	}
+}
